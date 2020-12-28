9f648752e02ae4ff28c85d7c62972440167d2ac5
muslimgift <76611044+muslimgift@users.noreply.github.com>
2020-12-28 23:14:31 +0500

    Delete task7.js

--- task7.js
+++
@@ -1,4 +0,0 @@
-document.write("Variable name can only contain number,$ and _.For example:$my_1stVariable "+"<br>")
-document.write("Variable must begin with a letter ,$ or _.For example :$name,_name or name"+"<br>")
-document.write("Variable name are case sensitive"+"<br>")
-document.write("Variable should not be js keyword "+"<br>")
